5bbfd9a44b0ed457ae82bdb99970d68d6ec9193e
Dave W <dave@effortwise.com>
2025-06-16 13:46:16 -0400

    Apply recent updates to Render and Functions index.js

--- Render/index.js
+++ Render/index.js
@@ -3609,7 +3609,7 @@ client.on(Events.InteractionCreate, async interaction => {
         const firstPromptEmbed = new EmbedBuilder()
             .setColor('#5865F2')
             .setTitle("🚀 Let's Set Your Experiment!")
-            .setDescription("\n\nExperiments have 3 components:\n\n1. **A Wish** to pursue\n2. **An outcome** to track\n3. **1 - 3 Habits** to test out\n\n✨ **Let's start with your wish!** ✨\n\nWhat's 1 thing you wish for in your daily life?\n\n**Examples:**\n● 'To be less stressed'\n● 'To have more energy'\n● 'To have better relationships'\n\n**Tap the <:chaticon:1384220348685488299> icon**\nand type your wish!\n\n<:chaticon:1384220348685488299> icon  →  →  →  ↘ ↘ ↘")
+            .setDescription("# Experiments have 3 components:\n\n## 1. **A Wish** to pursue\n## 2. **An outcome** to track\n## 3. **1 - 3 Habits** to test out\n\n## ✨ **Let's start with your wish!** ✨\n\n## What's 1 thing you wish for in your daily life?\n\n## **Examples:**\n## ● 'To be less stressed'\n## ● 'To have more energy'\n## ● 'To have better relationships'\n\n## **Tap the <:chaticon:1384220348685488299> icon**\n## and type your wish!\n\n### <:chaticon:1384220348685488299> icon  →  →  →  →  ↘  ↘  ↘")
 
         const promptMessage = await dmChannel.send({ embeds: [firstPromptEmbed] });
 
